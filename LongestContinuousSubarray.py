e92c3b2cd3b349f2692f23f016dc6d1211df7114
Mouli Dutta <117016206+mouli-dutta@users.noreply.github.com>
2024-06-23 06:08:50 +0530

    Create LongestContinuousSubarray.py

---
+++ LongestContinuousSubarray.py
@@ -0,0 +1,62 @@
+'''
+1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
+
+Given an array of integers nums and an integer limit, 
+return the size of the longest non-empty subarray such that 
+the absolute difference between any two elements of this subarray 
+is less than or equal to limit.
+ 
+
+Example 1:
+
+Input: nums = [8,2,4,7], limit = 4
+Output: 2 
+Explanation: All subarrays are: 
+[8] with maximum absolute diff |8-8| = 0 <= 4.
+[8,2] with maximum absolute diff |8-2| = 6 > 4. 
+[8,2,4] with maximum absolute diff |8-2| = 6 > 4.
+[8,2,4,7] with maximum absolute diff |8-2| = 6 > 4.
+[2] with maximum absolute diff |2-2| = 0 <= 4.
+[2,4] with maximum absolute diff |2-4| = 2 <= 4.
+[2,4,7] with maximum absolute diff |2-7| = 5 > 4.
+[4] with maximum absolute diff |4-4| = 0 <= 4.
+[4,7] with maximum absolute diff |4-7| = 3 <= 4.
+[7] with maximum absolute diff |7-7| = 0 <= 4. 
+Therefore, the size of the longest subarray is 2.
+Example 2:
+
+Input: nums = [10,1,2,4,7,2], limit = 5
+Output: 4 
+Explanation: The subarray [2,4,7,2] is the longest since the maximum absolute diff is |2-7| = 5 <= 5.
+Example 3:
+
+Input: nums = [4,2,2,2,4,4,2,2], limit = 0
+Output: 3
+'''
+
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        min_deque, max_deque = deque(), deque()
+        l = r = 0
+        ans = 0
+        while r < len(nums):
+            while min_deque and nums[r] <= nums[min_deque[-1]]:
+                min_deque.pop()
+            while max_deque and nums[r] >= nums[max_deque[-1]]:
+                max_deque.pop()
+            min_deque.append(r)
+            max_deque.append(r)
+            
+            while nums[max_deque[0]] - nums[min_deque[0]] > limit:
+                l += 1
+                if l > min_deque[0]:
+                    min_deque.popleft()
+                if l > max_deque[0]:
+                    max_deque.popleft()
+            
+            ans = max(ans, r - l + 1)
+            r += 1
+                
+        return ans
+
+
